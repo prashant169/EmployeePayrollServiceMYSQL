6365724011693d31de2b66cc6efa013c27f1c89a
prashant169 <prashantphad2018@gmail.com>
2021-10-28 18:57:24 -0800

    Added:Creating a Employee_payrole Table in Database

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -1,5 +1,16 @@
-
+UC1..
 create database payroll_services;
 show databases;
 use payroll_services;
 select database();
+
+UC2..
+
+create table employee_payroll
+( id int not null auto_increment ,
+name varchar(50) not null,
+salary Double not null,
+startDate Date not null,
+primary key(id)
+);
+
