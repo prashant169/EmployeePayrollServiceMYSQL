2dac74fe6f7ca923cf836ad31f7085350a0f613a
prashant169 <prashantphad2018@gmail.com>
2021-10-28 19:45:51 -0800

    Added: Calculate sum ,average ,min,max

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -49,6 +49,11 @@ UPDATE employee_payroll set gender = 'M' WHERE id =4;
 
 select * from employee_payroll;
 
+UC7 
+select gender,sum(salary) from employee_payroll Where gender = 'M' group by gender;
+select gender,min(salary) from employee_payroll;
+select gender,max(salary) from employee_payroll;
+select gender,avg(salary) from employee_payroll;
+select count(salary) from employee_payroll;
 
-
-
+ 
